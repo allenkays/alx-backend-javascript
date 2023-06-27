d56ce66c411566fc5a9ac3f48977d882d89a107d
allenkays <allenkays@tutanota.com>
2023-06-27 23:12:35 +0300

    Add space before colon

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -5,9 +5,9 @@ function getCurrentYear() {
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
   const budget = {
-    [`income - ${getCurrentYear()}`]:income,
-    [`gdp - ${getCurrentYear()}`]: gdp,
-    [`capita - ${getCurrentYear()}`]: gdp,
+    [`income - ${getCurrentYear()}`] : income,
+    [`gdp - ${getCurrentYear()}`] : gdp,
+    [`capita - ${getCurrentYear()}`] : gdp,
   };
 
   return budget;
