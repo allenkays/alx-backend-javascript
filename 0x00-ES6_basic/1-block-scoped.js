c1f82f83a320e910b0739c83cfabc3571d834471
allenkays <allenkays@tutanota.com>
2023-06-27 10:25:53 +0300

    Change var to let

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,10 @@
+export default function taskBlock(trueOrFalse) {
+  let task = false;
+  let task2 = true;
+
+  if (trueOrFalse) {
+    task = true;
+    task3 = false;
+  }
+  return [task, task2];
+}
