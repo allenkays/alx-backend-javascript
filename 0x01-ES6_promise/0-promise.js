e679262449ab152895b33479b0f66acccd39a89a
allenkays <allenkays@tutanota.com>
2023-06-29 00:35:03 +0300

    Fix lint errors

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,7 @@
 function getResponseFromAPI() {
-  return new Promise ((resolve, reject)=> {
-    setTimeout(()=> {
-      const response = "Sample API response"
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const response = 'Sample API response';
       resolve(response);
     }, 2000);
   });
