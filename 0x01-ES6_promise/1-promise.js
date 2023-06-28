fe24954573623e211cbff83a0f9d6edca3aca9b5
allenkays <allenkays@tutanota.com>
2023-06-28 23:51:38 +0300

    Return a Promise with boolean params

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,16 @@
+function getFullResponseFromAPI(success) {
+  return new Promise ((resolve, reject)=> {
+    if (success) {
+      const response = {
+        status: 200,
+        body: "success",
+      };
+      resolve(response)
+    } else {
+      const error =  new Error("The fake API is not working currently");
+      reject(error); 
+    }
+  });
+}
+
+export default getFullResponseFromAPI;
