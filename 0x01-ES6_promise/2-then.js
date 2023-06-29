50fb7852c8f3d08a42d4a5b1d5c7530952c10b45
allenkays <allenkays@tutanota.com>
2023-06-29 10:48:49 +0300

    Include a finally callback in function

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,12 @@
 function handleResponseFromAPI(promise) {
-  promise
-    .then(() => {
+  return promise
+    .then((response) => {
       console.log('Got a response from the API');
       return { status: 200, body: 'Success' };
     })
-    .catch(() => {
+    .catch(() => new Error {
+    .finaly(() => {
       console.log('Got a response from the API');
-      return new Error();
     });
 }
 
