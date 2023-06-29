8765cc79290b0db520c1ccd8b288f4329b5ac2b0
allenkays <allenkays@tutanota.com>
2023-06-29 11:20:59 +0300

    Fix response for then callback

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,6 @@
 function handleResponseFromAPI(promise) {
-  return promise
-    .then(() => {
-      console.log('Got a response from the API');
-      return { status: 200, body: 'Success' };
-    })
+  return promise.then(() => (
+    { status: 200, body: 'Success' }))
     .catch(() => new Error())
     .finally(() => {
       console.log('Got a response from the API');
