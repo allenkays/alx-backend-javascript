468a99cec83c6d3b4e6893522cbb5d547d2a1d60
allenkays <allenkays@tutanota.com>
2023-06-30 12:02:16 +0300

    Fix lint error

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,25 +1,26 @@
-class HolbertonCourse {
+export default class HolbertonCourse {
   constructor(name, length, students) {
     this._name = this.validateString(name);
     this._length = this.validateNumber(length);
     this._students = this.validateStudents(students);
   }
-    validateString(value) {
-    if (typeof value !== 'string') {
+
+  validateString(value) {
+    if (typeof this._name !== 'string') {
       throw new TypeError('Name must be a string');
     }
     return value;
   }
 
   validateNumber(value) {
-    if (typeof value !== 'number') {
+    if (typeof this._length !== 'number') {
       throw new TypeError('Length must be a number.');
     }
     return value;
   }
 
   validateStudents(value) {
-    if (!Array.isArray(value) || !value.every((item) => typeof item === 'string')) {
+    if (!Array.isArray(this._students) || !this._students.every((item) => typeof item === 'string')) {
       throw new TypeError(' Students must be an array of strings.');
     }
     return value;
@@ -49,5 +50,3 @@ class HolbertonCourse {
     this._students = this.validateStudents(value);
   }
 }
-
-export default HolbertonCourse;
