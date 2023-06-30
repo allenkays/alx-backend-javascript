6b18a5acea0f70bea98437a77ceac18a3093d10a
allenkays <allenkays@tutanota.com>
2023-06-30 11:38:47 +0300

    Edit Error ouput string and class definitionwq

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,26 +1,26 @@
-Class HolbertonCourse {
+class HolbertonCourse {
   constructor(name, length, students) {
     this._name = this.validateString(name);
     this._length = this.validateNumber(length);
-    this.students = this.validateStudents(students);
+    this._students = this.validateStudents(students);
   }
     validateString(value) {
     if (typeof value !== 'string') {
-      throw new TypeError('Invalid type. Expected a string.');
+      throw new TypeError('Name must be a string');
     }
     return value;
   }
 
   validateNumber(value) {
     if (typeof value !== 'number') {
-      throw new TypeError('Invalid type. Expected a number.');
+      throw new TypeError('Length must be a number.');
     }
     return value;
   }
 
   validateStudents(value) {
     if (!Array.isArray(value) || !value.every((item) => typeof item === 'string')) {
-      throw new TypeError('Invalid type. Expected an array of strings.');
+      throw new TypeError(' Students must be an array of strings.');
     }
     return value;
   }
