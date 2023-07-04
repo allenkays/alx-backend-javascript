0a86ed5fcd51ae373cf8452545e4db7528d2f8c3
allenkays <allenkays@tutanota.com>
2023-07-04 21:36:22 +0300

    Create function to return a sliced set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,11 @@
+export default function cleanSet(set, startString) {
+  if (startString === '') {
+    return '';
+  }
+  const result = Array.from(set)
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(startString.length))
+    .join('-');
+
+  return result;
+}
