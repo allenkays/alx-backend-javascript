e1d2eeca4d8d8d8b305edb01bfbe70840c748f3b
allenkays <allenkays@tutanota.com>
2023-08-22 19:21:28 +0300

    Add newline to print statement

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -12,7 +12,7 @@ program.on('line', (username) => {
 });
 
 program.on('close', () => {
-  console.log('This important software is now closing');
+  console.log('This important software is now closing\n');
 });
 
 module.export = program;
