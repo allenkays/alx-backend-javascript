5a274ac0401b0087f3a6cdd5b536324830750880
allenkays <allenkays@tutanota.com>
2023-08-27 20:55:15 +0300

    Testing api response

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,19 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('Index page', () => {
+  const baseUrl = 'http://localhost:7865';
+  it('correct status code?', (done) => {
+    request.get(baseUrl, (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('correct result', (done) => {
+    request.get(baseUrl, (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
