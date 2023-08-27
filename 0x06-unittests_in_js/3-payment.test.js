19a87a140fe8df8f92b2ea68b034ac0d2efff8b9
allenkays <allenkays@tutanota.com>
2023-08-27 20:04:18 +0300

    Add test using spies sinon

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,17 @@
+const sinon = require('sinon');
+const expect = require('chai').expect;
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  it('should call calculateNumber with correct arguments and display result', () => {
+    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(calculateNumberSpy.calledOnce).to.be.true;
+    expect(calculateNumberSpy.calledWith('SUM', 100, 20)).to.be.true;
+
+    calculateNumberSpy.restore();
+  });
+});
---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,22 @@
+const Utils = {
+  calculateNumber(type, a, b) {
+    const numA = Math.round(a);
+    const numB = Math.round(b);
+
+    if (type === 'SUM') {
+      return numA + numB;
+    } else if (type === 'SUBTRACT') {
+      return numA - numB;
+    } else if (type === 'DIVIDE') {
+      if (numB === 0) {
+        return 'Error';
+      }
+
+      return numA / numB;
+    } else {
+      throw new Error('Invalid type');
+    }
+  }
+};
+
+module.exports = Utils;
